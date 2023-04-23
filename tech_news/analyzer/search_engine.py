4990247b56d422791c1231df893e179d8446a348
Milena Suzumura <milena.suzumura11@gmail.com>
2023-04-23 12:07:03 -0300

    requisito 8 e 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import find_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -8,14 +9,31 @@ def search_by_title(title):
     for new in news:
         if (title.lower() in new['title'].lower()):
             list_news.append((new['title'], new['url']))
-    return news
+    return list_news
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        news = find_news()
+        list_news = []
+        date_format = (
+            datetime
+            .strptime(date, '%Y-%m-%d').strftime('%d-%m-%Y').replace('-', '/')
+        )
+        for new in news:
+            if (date_format.lower() in new['timestamp'].lower()):
+                list_news.append((new['title'], new['url']))
+        return list_news
+    except ValueError:
+        raise ValueError('Data inválida')
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = find_news()
+    list_news = []
+    for new in news:
+        if (category.lower() in new['category'].lower()):
+            list_news.append((new['title'], new['url']))
+    return list_news
