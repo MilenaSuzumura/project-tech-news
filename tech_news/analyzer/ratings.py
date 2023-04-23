c5c3d4deeff2cbee5965efb946075aa1a0a54523
Milena Suzumura <milena.suzumura11@gmail.com>
2023-04-23 12:34:49 -0300

    requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,17 @@
+from tech_news.database import find_news
+from collections import Counter
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    list_category = []
+    for new in news:
+        list_category.append(new['category'])
+    top_categories = Counter(sorted(list_category)).most_common(5)
+    list_top_category = []
+
+    for category in top_categories:
+        list_top_category.append(category[0])
+
+    return list_top_category
